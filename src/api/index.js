304be7f0743a424531e0bc35129cc17d0b4786cf
MahmutYnc <mahmutla33@gmail.com>
2020-10-11 01:18:19 +0300

    axios needed for fetch data from api

---
+++ src/api/index.js
@@ -0,0 +1,17 @@
+/*axios({
+    "method": "GET",
+    "url": "https://quotes15.p.rapidapi.com/quotes/random/",
+    "headers": {
+      "content-type": "application/octet-stream",
+      "x-rapidapi-host": "quotes15.p.rapidapi.com",
+      "x-rapidapi-key": process.env.REACT_APP_API_KEY
+    }, "params": {
+      "language_code": "en"
+    }
+  })
+  .then((response) => {
+    setResponseData(response.data)
+  })
+  .catch((error) => {
+    console.log(error)
+  }) */
